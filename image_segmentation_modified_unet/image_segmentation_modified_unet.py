9bdd8293ed30496e507d7463606a10a1f864cc7c
张凌峰 <zhanglingfeng@zhanglingfengdeMacBook-Pro.local>
2020-04-01 11:29:41 -0400

    02020401am

---
+++ image_segmentation_modified_unet/image_segmentation_modified_unet.py
@@ -0,0 +1,91 @@
+# https://www.tensorflow.org/tutorials/images/segmentation
+
+# pip install -q git+https://github.com/tensorflow/examples.git
+
+import tensorflow as tf
+from tensorflow_examples.models.pix2pix import pix2pix
+
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+
+dataset, info = tfds.load('oxford_iiit_pet:3.*.*', with_info=True)
+
+def normalize(input_image, input_mask):
+  input_image = tf.cast(input_image, tf.float32) / 255.0
+  input_mask -= 1
+  return input_image, input_mask
+
+@tf.function
+def load_image_train(datapoint):
+  input_image = tf.image.resize(datapoint['image'], (128, 128))
+  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+
+  if tf.random.uniform(()) > 0.5:
+    input_image = tf.image.flip_left_right(input_image)
+    input_mask = tf.image.flip_left_right(input_mask)
+
+  input_image, input_mask = normalize(input_image, input_mask)
+
+  return input_image, input_mask
+
+def load_image_test(datapoint):
+  input_image = tf.image.resize(datapoint['image'], (128, 128))
+  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+
+  input_image, input_mask = normalize(input_image, input_mask)
+
+  return input_image, input_mask
+
+TRAIN_LENGTH = info.splits['train'].num_examples
+BATCH_SIZE = 64
+BUFFER_SIZE = 1000
+STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
+
+train = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+test = dataset['test'].map(load_image_test)
+
+train_dataset = train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+test_dataset = test.batch(BATCH_SIZE)
+
+def display(display_list):
+  plt.figure(figsize=(15, 15))
+
+  title = ['Input Image', 'True Mask', 'Predicted Mask']
+
+  for i in range(len(display_list)):
+    plt.subplot(1, len(display_list), i+1)
+    plt.title(title[i])
+    plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+    plt.axis('off')
+#   plt.show()
+    plt.savefig('image_example.png')
+
+for image, mask in train.take(1):
+  sample_image, sample_mask = image, mask
+display([sample_image, sample_mask])
+
+base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
+
+# Use the activations of these layers
+layer_names = [
+    'block_1_expand_relu',   # 64x64
+    'block_3_expand_relu',   # 32x32
+    'block_6_expand_relu',   # 16x16
+    'block_13_expand_relu',  # 8x8
+    'block_16_project',      # 4x4
+]
+layers = [base_model.get_layer(name).output for name in layer_names]
+
+# Create the feature extraction model
+down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
+
+down_stack.trainable = False
+
+
+
+
+
