cea012d80a603b7c6ee399b807165fb3155ca1c7
张凌峰 <zhanglingfeng@zhanglingfengdeMacBook-Pro.local>
2020-04-02 23:53:02 -0400

    segmentation_unet

--- image_segmentation_modified_unet/image_segmentation_modified_unet.py
+++ image_segmentation_modified_unet/image_segmentation_modified_unet.py
@@ -11,6 +11,10 @@ tfds.disable_progress_bar()
 from IPython.display import clear_output
 import matplotlib.pyplot as plt
 
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+print("physical_devices-------------", len(physical_devices))
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 dataset, info = tfds.load('oxford_iiit_pet:3.*.*', with_info=True)
 
 def normalize(input_image, input_mask):
@@ -51,7 +55,7 @@ train_dataset = train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
 train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 test_dataset = test.batch(BATCH_SIZE)
 
-def display(display_list):
+def display(display_list,file_name):
   plt.figure(figsize=(15, 15))
 
   title = ['Input Image', 'True Mask', 'Predicted Mask']
@@ -62,11 +66,13 @@ def display(display_list):
     plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
     plt.axis('off')
 #   plt.show()
-    plt.savefig('image_example.png')
+    plt.savefig(file_name)
 
 for image, mask in train.take(1):
   sample_image, sample_mask = image, mask
-display([sample_image, sample_mask])
+display([sample_image, sample_mask],'image_example.png')
+
+OUTPUT_CHANNELS = 3
 
 base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
 
@@ -83,9 +89,95 @@ layers = [base_model.get_layer(name).output for name in layer_names]
 # Create the feature extraction model
 down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
 
+# the encoder will not be trained during the training process
 down_stack.trainable = False
 
+up_stack = [
+    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
+    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
+    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
+    pix2pix.upsample(64, 3),   # 32x32 -> 64x64
+]
 
+def unet_model(output_channels):
+  inputs = tf.keras.layers.Input(shape=[128, 128, 3])
+  x = inputs
 
+  # Downsampling through the model
+  skips = down_stack(x)
+  x = skips[-1]
+  skips = reversed(skips[:-1])
+
+  # Upsampling and establishing the skip connections
+  for up, skip in zip(up_stack, skips):
+    x = up(x)
+    concat = tf.keras.layers.Concatenate()
+    x = concat([x, skip])
+
+  # This is the last layer of the model
+  last = tf.keras.layers.Conv2DTranspose(
+      output_channels, 3, strides=2,
+      padding='same')  #64x64 -> 128x128
+
+  x = last(x)
+
+  return tf.keras.Model(inputs=inputs, outputs=x)
+
+# the network is trying to assign each pixel a label, just like multi-class prediction
+
+model = unet_model(OUTPUT_CHANNELS)
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+tf.keras.utils.plot_model(model, show_shapes=True)
+
+def create_mask(pred_mask):
+  pred_mask = tf.argmax(pred_mask, axis=-1)
+  pred_mask = pred_mask[..., tf.newaxis]
+  return pred_mask[0]
+
+def show_predictions(dataset=None, num=1, file_name='wrong_file_name.png'):
+  if dataset:
+    for image, mask in dataset.take(num):
+      pred_mask = model.predict(image)
+      display([image[0], mask[0], create_mask(pred_mask)],file_name)
+  else:
+    display([sample_image, sample_mask,
+             create_mask(model.predict(sample_image[tf.newaxis, ...]))],file_name)
+
+show_predictions(file_name='prediction_without_training.png')
+
+class DisplayCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs=None):
+    clear_output(wait=True)
+    show_predictions(file_name='prediction_after_epoch_'+str(epoch+1)+'.png')
+    print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
+
+EPOCHS = 20
+VAL_SUBSPLITS = 5
+VALIDATION_STEPS = info.splits['test'].num_examples//BATCH_SIZE//VAL_SUBSPLITS
+
+model_history = model.fit(train_dataset, epochs=EPOCHS,
+                          steps_per_epoch=STEPS_PER_EPOCH,
+                          validation_steps=VALIDATION_STEPS,
+                          validation_data=test_dataset,
+                          callbacks=[DisplayCallback()])
+
+loss = model_history.history['loss']
+val_loss = model_history.history['val_loss']
+
+epochs = range(EPOCHS)
+
+plt.figure()
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'bo', label='Validation loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss Value')
+plt.ylim([0, 1])
+plt.legend()
+# plt.show()
+plt.savefig("loss_plot.png")
 
 
